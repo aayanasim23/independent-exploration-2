4b48e552422224d62aee6b4c7bc80e391bc88fe5
Aayan Asim <aayan.asim23@gmail.com>
2024-08-14 11:43:33 -0400

    Fixed Counter

--- intervals/script.js
+++ intervals/script.js
@@ -3,3 +3,11 @@ let time = 0;
 // reference to the <span> containing the number
 let counterElement = document.getElementById("time");
 
+
+//Added code is below
+function timeupdate() {
+    time += 1;
+    counterElement.textContent = time;
+}
+
+setInterval(timeupdate, 1000);
